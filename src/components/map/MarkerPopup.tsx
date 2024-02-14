862c72499e06d068481dd6718b85c6ac22923d1a
kim <kim.phanhoang@epfl.ch>
2024-02-14 10:19:41 +0100

    refactor: show address

--- src/components/map/MarkerPopup.tsx
+++ src/components/map/MarkerPopup.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from 'react';
 import { Popup } from 'react-leaflet';
 
 import { Stack, Typography } from '@mui/material';
 
 import { DiscriminatedItem, ItemGeolocation } from '@graasp/sdk';
 
-import notifier from '../../config/notifier';
-import { axios } from '../../config/queryClient';
+import { hooks } from '../../config/queryClient';
 import DeleteItemButton from './DeleteItemButton';
 import DeleteLocationButton from './DeleteLocationButton';
 import ViewButton from './ViewButton';
@@ -22,25 +20,7 @@ const MarkerPopup = ({
   lat: ItemGeolocation['lat'];
   lng: ItemGeolocation['lng'];
 }): JSX.Element => {
-  const [address, setAddress] = useState<string | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      await axios
-        .get<{ display_name: string }>(
-          'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=47.217954&lon=-1.552918',
-          {
-            responseType: 'json',
-          },
-        )
-        .then(({ data }) => {
-          setAddress(data.display_name);
-        })
-        .catch((e) => {
-          notifier(e);
-        });
-    })();
-  }, [lat, lng]);
+  const { data: address } = hooks.useAddressFromGeolocation({ lat, lng });
 
   const { name, description } = item;
 
@@ -49,7 +29,7 @@ const MarkerPopup = ({
       <Typography variant="h5">{name.slice(0, NAME_MAX_LENGTH)}</Typography>
       {/* TODO: slice and show html  */}
       <Typography variant="subtitle1">{description}</Typography>
-      <Typography variant="caption">{address}</Typography>
+      <Typography variant="caption">{address?.display_name}</Typography>
       <Stack direction="row">
         <ViewButton item={item} />
         <DeleteLocationButton item={item} />
