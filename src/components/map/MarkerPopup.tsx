942015274af287c403470bb871fbf5de37b1539b
kim <kim.phanhoang@epfl.ch>
2024-02-26 17:54:18 +0100

    refactor: use addressLabel

--- src/components/map/MarkerPopup.tsx
+++ src/components/map/MarkerPopup.tsx
@@ -2,9 +2,8 @@ import { Popup } from 'react-leaflet';
 
 import { Stack, Typography } from '@mui/material';
 
-import { DiscriminatedItem, ItemGeolocation } from '@graasp/sdk';
+import { ItemGeolocation } from '@graasp/sdk';
 
-import { useQueryClientContext } from '../context/QueryClientContext';
 import DeleteItemButton from './DeleteItemButton';
 import DeleteLocationButton from './DeleteLocationButton';
 import ViewButton from './ViewButton';
@@ -12,24 +11,20 @@ import ViewButton from './ViewButton';
 const NAME_MAX_LENGTH = 20;
 
 const MarkerPopup = ({
-  item,
-  lat,
-  lng,
+  geolocation,
 }: {
-  item: DiscriminatedItem;
-  lat: ItemGeolocation['lat'];
-  lng: ItemGeolocation['lng'];
+  geolocation: ItemGeolocation;
 }): JSX.Element => {
-  const { useAddressFromGeolocation } = useQueryClientContext();
-  const { data: address } = useAddressFromGeolocation({ lat, lng });
-  const { name, description } = item;
+  const { item } = geolocation;
 
   return (
     <Popup>
-      <Typography variant="h5">{name.slice(0, NAME_MAX_LENGTH)}</Typography>
+      <Typography variant="h5">
+        {item.name.slice(0, NAME_MAX_LENGTH)}
+      </Typography>
       {/* TODO: slice and show html  */}
-      <Typography variant="subtitle1">{description}</Typography>
-      <Typography variant="caption">{address?.display_name}</Typography>
+      <Typography variant="subtitle1">{item.description}</Typography>
+      <Typography variant="caption">{geolocation.addressLabel}</Typography>
       <Stack direction="row">
         <ViewButton item={item} />
         <DeleteLocationButton item={item} />
