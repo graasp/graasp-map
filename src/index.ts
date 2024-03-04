fc2b1fa46818cc77745fbbc673ff776687782c1a
kim <kim.phanhoang@epfl.ch>
2024-03-04 16:06:59 +0100

    refactor: export geolocpicker props

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Map } from './components/Map';
 export { default as GeolocationPicker } from './components/GeolocationPicker/GeolocationPicker';
+export type { GeolocationPickerProps } from './components/GeolocationPicker/GeolocationPicker';
 
 export * from './types';
